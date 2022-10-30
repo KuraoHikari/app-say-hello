0f9693f23837551c35a2070bab43db179aa99806
KuraoHikari <dewaindra705@gmail.com>
2022-10-30 22:24:06 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KuraoHikari/app-say-hello
+
+go 1.19
+
+require github.com/KuraoHikari/golang-menengah/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/KuraoHikari/golang-menengah/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello(" anjay"))
+}
